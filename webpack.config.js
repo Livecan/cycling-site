df60f4b8e98f0fcb7ee7e6bba6b430171ce7fde2
Livecan <roman.meszaros@gmail.com>
2021-12-28 15:07:16 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const isDevMode = process.env.NODE_ENV !== 'production';
+
+const config = {
+  entry: {
+    main: ["./src/index.jsx"]
+  },
+  devtool: (isDevMode) ? 'source-map' : false,
+  mode: (isDevMode) ? 'development' : 'production',
+  output: {
+    path: isDevMode ? path.resolve(__dirname, "dist_dev") : path.resolve(__dirname, "dist"),
+    filename: '[name].min.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.gpx$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = config;
