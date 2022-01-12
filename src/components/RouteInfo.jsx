30b8eb4d831a57ec99f6957b1b13801d9ce44889
Livecan <roman.meszaros@gmail.com>
2022-01-12 15:29:32 +0200

    GPX download link

--- src/components/RouteInfo.jsx
+++ src/components/RouteInfo.jsx
@@ -1,23 +1,32 @@
-import { Box, Divider, Stack, Typography } from "@mui/material";
+import { Box, Divider, Link, Stack, Typography } from "@mui/material";
 import React from "react";
+import { MdOutlineFileDownload } from 'react-icons/md';
 
+const lineBoxStyle = {display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}
 
 export default function RouteInfo(props) {
-
   const simpleData = [
     {label: 'Distance', value: `${props.distance.toFixed(2)} km`},
-    {label: 'Speed', value: props.speed},
-    {label: 'Elevation gain', value: `${props.elevation} m`}
+    {label: 'Speed', value: props.speedIndex},
+    {label: 'Climb', value: props.climbIndex},
+    {label: 'Elevation gain', value: `${props.elevation.toFixed(0)} m`}
   ];
 
   return (
     <Stack divider={<Divider />}>
       {simpleData.map(info =>
-        <Box key={info.label} sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-          <Typography variant="body1" align="left">{info.label}</Typography>
-          <Typography variant="body1" align="right">{info.value}</Typography>
+        <Box key={info.label} sx={lineBoxStyle}>
+          <Typography variant="body1">{info.label}</Typography>
+          <Typography variant="body1">{info.value}</Typography>
         </Box>
       )}
+      <Box sx={lineBoxStyle}>
+        <Typography variant="body1">Download GPX</Typography>
+        <Link href={props.download}>
+          <MdOutlineFileDownload />
+        </Link>
+
+      </Box>
       <Typography variant="body1">Description:</Typography>
       <Typography variant="body1">{props.description}</Typography>
     </Stack>
